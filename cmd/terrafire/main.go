a0a77c2a89ace05f638c01605e98d2d4075001db
kimiyash <kimiyash@gmail.com>
2020-03-04 12:10:41 +0900

    Get github archive link

--- cmd/terrafire/main.go
+++ cmd/terrafire/main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/mitene/terrafire"
-	"log"
-)
+import "github.com/mitene/terrafire"
 
 func main() {
-	v, err := terrafire.LoadConfig("./sample")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%#v\n", v)
-	fmt.Println(v.Terrafire.Backend.Name)
-	fmt.Println(v.Terrafire.Backend.Bucket)
-	fmt.Println(v.Terrafire.Backend.Key)
-
-	fmt.Println(v.TerraformDeploy[0].Name)
-	fmt.Println(v.TerraformDeploy[0].Source.Owner)
-	fmt.Println(v.TerraformDeploy[0].Vars)
-
-	fmt.Println(v.TerraformDeploy[1].Name)
-	fmt.Println(v.TerraformDeploy[1].Source.Owner)
-	fmt.Println(v.TerraformDeploy[1].Vars)
+	terrafire.GetSource("mitene", "terrafire", "")
 }
--- github.go
+++ github.go
@@ -1,5 +1,17 @@
 package terrafire
 
 import (
+	"context"
+	"fmt"
 	"github.com/google/go-github/v29/github"
 )
+
+func GetSource(owner string, repo string, ref string) error {
+	client := github.NewClient(nil)
+	url, _, err := client.Repositories.GetArchiveLink(context.Background(), owner, repo, github.Zipball, nil, true)
+	if err != nil {
+		return err
+	}
+	fmt.Println(url)
+	return nil
+}
